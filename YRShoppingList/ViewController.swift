77a6a8490b5c5b6cbb81a1adf58e9b13abe309d8
inexcii <ralph.zhouyuan@gmail.com>
2020-05-04 16:27:25 +0900

    make item visible when keyboard shows up

--- YRShoppingList/ViewController.swift
+++ YRShoppingList/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
 
         collectionView.register(UINib(nibName: "ListItemCell", bundle: nil), forCellWithReuseIdentifier: "CustomCell")
 
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
 }
@@ -88,5 +91,20 @@ extension ViewController {
         })
         alert.addAction(cancel)
         self.present(alert, animated: true, completion: nil)
+    }
+
+    @objc private func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            collectionView.contentInset = .zero
+        } else {
+            collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        collectionView.scrollIndicatorInsets = collectionView.contentInset
     }
 }
